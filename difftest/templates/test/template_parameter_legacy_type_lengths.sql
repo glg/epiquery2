4e8f1664318854be003efaca8a1c4bbef9e09fdc
Ian Groff <ian@iangroff.com>
2020-02-21 17:28:18 -0600

    Adding a test for the length, precision, cale values of the legacy behavior. This is being checked in brokena and will actually need to be run with a legacy version of epiquery2 to make sure we're really comparing the results as they are in the legacy version to what is being generated now.

---
+++ difftest/templates/test/template_parameter_legacy_type_lengths.sql
@@ -0,0 +1,28 @@
+/*
+executionMasks:
+  a_valid_bitmask: 1
+
+--@shortVarchar varchar
+--@longVarchar varchar
+--@shortNVarchar nvarchar
+--@longNVarchar nvarchar
+--@aDecimal decimal
+--@aNumeric numeric
+--@aDateTime2 datetime2
+--@aTime time
+--@aDateTimeOffset datetimeoffset
+
+
+*/
+  select 
+    -- this relies on the knowledge that epiquery prepends a comment to the exeucting query that contains the
+    -- template path information. So we're taking everything up to the template path that epi is prepending
+    -- thus in the case of a parameterized query (executed via sp_executesql) this first portion will be the param 
+    -- declaration
+    LEFT(st.text, CHARINDEX('-- ', st.text)-1) queryParamText
+  from
+    sys.dm_exec_requests as er
+    cross apply sys.dm_exec_query_plan(er.plan_handle) as qp
+    cross apply sys.dm_exec_sql_text(er.plan_handle) as st
+  where 
+   session_id = @@spid
