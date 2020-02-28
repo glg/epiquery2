0e095865d6c83017cfd9ac756d61fb1a475730aa
Ian Groff <ian@iangroff.com>
2020-02-27 19:33:30 -0600

    adding a test to prove that we get the same response for legacy parmater (no length, precision or scale values) that we got from the old version of epi. The exepcted result was gathered by running the template on an old production version of epi

--- difftest/templates/test/template_parameter_nchar_type_lengths.sql
+++ difftest/templates/test/template_parameter_nchar_type_lengths.sql
@@ -12,7 +12,7 @@ executionMasks:
 
 
 */
-  select 
+  select
     -- this relies on the knowledge that epiquery prepends a comment to the exeucting query that contains the
     -- template path information. So we're taking everything up to the template path that epi is prepending
     -- thus in the case of a parameterized query (executed via sp_executesql) this first portion will be the param 
@@ -22,5 +22,5 @@ executionMasks:
     sys.dm_exec_requests as er
     cross apply sys.dm_exec_query_plan(er.plan_handle) as qp
     cross apply sys.dm_exec_sql_text(er.plan_handle) as st
-  where 
+  where
    session_id = @@spid
